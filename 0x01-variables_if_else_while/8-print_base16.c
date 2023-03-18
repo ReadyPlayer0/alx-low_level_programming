e82e79b16530bc720b66667ad777b45997d90f0f
Adeyimika <adetunjiagboola@yahoo.com>
2023-03-17 17:47:32 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,14 +8,14 @@ int main(void)
 {
 int i;
 char c;
-for (i = 0; i < 10; i++)
+    for (i = 0; i < 10; i++)
 {
-    putchar(i + '0');
+putchar(i + '0');
 }
-for (c = 'a'; c <= 'f'; c++)
+    for (c = 'a'; c <= 'f'; c++)
 {
-    putchar(c);
+putchar(c);
 }
-putchar('\n');
+    putchar('\n');
 return (0);
 }
