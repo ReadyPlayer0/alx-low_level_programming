2fc0acd50d15caa9ca83883800266c54ad053cb2
Adeyimika <adetunjiagboola@yahoo.com>
2023-03-21 15:36:39 -0700

    done

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+void print_alphabet(void);
+
+void print_times_table(int n);
+
+int _isalpha(int c);
+
+int _abs(int);
+
+int _islower(int c);
+
+void print_to_98(int n);
+
+int add(int, int);
+
+int _putchar(char c);
+
+void print_last_digit(int);
+
+int print_sign(int n);
+
+void times_table(void);
+
+void jack_bauer(void);
+
+void print_alphabet_x10(void);
