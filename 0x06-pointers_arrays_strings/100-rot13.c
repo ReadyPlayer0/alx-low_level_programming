40b8104d3e94fdd314c7d5665353e8922ca7ea32
Adeyimika <adetunjiagboola@yahoo.com>
2023-03-30 15:37:47 -0700

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,22 +1,31 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer type
- * i - integer type
+ * rot13 - Encodes a string using rot13
+ * @str: Pointer to a string to be encoded
+ *
+ * Return: The encoded string
  */
-void print_number(int n)
+char *rot13(char *str)
 {
-	unsigned int num;
+	int i, j;
+	char *s1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *s2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	if (n < 0)
+	/* iterate over each character in the string */
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar('-');
-		n = -n;
-		num = n;
+		/* iterate over each character in the substitution strings */
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			/* if the current character matches a character in the first string, substitute it */
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+				break;
+			}
+		}
 	}
-	num = n;
-	if (num / 10)
-		print_number(num / 10);
-	_putchar(num % 10 + '0');
+
+	return (str);
 }
