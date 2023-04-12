33404ac0674935c78dab7bd70f74225332a710da
Adetunji Adeyimika <adadet@taltech.ee>
2023-04-12 04:15:37 +0300

    Betty Error line 6

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 /**
- * create_array - Creates an array of chars and initializes it with a specific char
+ * create_array - Creates an array of chars and initializes.
  *
  * @size: The size of the array
  * @c: The char to initialize the array with
