9751c448ff7828c1e1bc30a185dc05fe56390533
Adetunji Adeyimika <adadet@taltech.ee>
2023-04-12 04:13:11 +0300

    Variable Declaration 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,24 +3,27 @@
 #include <stddef.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char.
- * @size: the size of the array
- * @c: the specific char to initialize the array with
+ * create_array - Creates an array of chars and initializes it with a specific char
+ *
+ * @size: The size of the array
+ * @c: The char to initialize the array with
  *
  * Return: Pointer to the array, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
-  if (size == 0)
-    return (NULL);
+	char *arr;
+	unsigned int i;
 
-  char *arr = malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
 
-  if (arr == NULL)
-    return (NULL);
+	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (NULL);
 
-  for (unsigned int i = 0; i < size; i++)
-    arr[i] = c;
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 
-  return (arr);
+	return (arr);
 }
