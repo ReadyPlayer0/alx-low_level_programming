48e987223a76150e223b0f7ae4d9610a00ea210d
Adetunji Adeyimika <adadet@taltech.ee>
2023-04-12 04:23:09 +0300

    Indentation errors

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,35 +2,36 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
+ * str_concat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
  *
- * @s1: The first string.
- * @s2: The second string.
- *
- * Return: A pointer to the concatenated string, or NULL on failure.
+ * Return: Pointer to a newly allocated space in memory containing
+ * the concatenated string or NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
+    int len1, len2, i, j;
     char *concat_str;
-    int len1 = 0, len2 = 0, i = 0, j = 0;
 
     if (s1 == NULL)
         s1 = "";
     if (s2 == NULL)
         s2 = "";
 
-    while (s1[len1])
-        len1++;
-    while (s2[len2])
-        len2++;
+    for (len1 = 0; s1[len1] != '\0'; len1++)
+        ;
+    for (len2 = 0; s2[len2] != '\0'; len2++)
+        ;
 
     concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
     if (concat_str == NULL)
         return (NULL);
 
-    for (i = 0; s1[i]; i++)
+    for (i = 0; i < len1; i++)
         concat_str[i] = s1[i];
-    for (j = 0; s2[j]; j++)
+
+    for (j = 0; j < len2; j++)
         concat_str[i + j] = s2[j];
 
     concat_str[i + j] = '\0';
