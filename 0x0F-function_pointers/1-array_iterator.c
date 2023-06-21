6ee48ff830c36ca7973a8279c7e34e21a86fc1a9
Adetunji Adeyimika <adadet@taltech.ee>
2023-06-22 02:25:29 +0300

    Done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,63 +1,29 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
- * array_iterator - Executes a function on each element of an array.
- * @array: Pointer to the array.
+ * array_iterator - maps an array through a function pointer
+ * @array: Pointer to the int array.
  * @size: Size of the array.
- * @action: Pointer to the function to be executed on each element.
+ * @action: Function pointer.
  *
  * This function iterates over each element of the array and applies the
- * provided function to each element.
+ * provided function pointer to each element.
  *
  * @array: Pointer to the array of integers.
  * @size: Size of the array.
- * @action: Pointer to the function with the signature `void (*action)(int)`,
- *          to be executed on each element of the array.
+ * @action: Function pointer with the signature `void (*action)(int)`.
  *
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array && action)
+    int *end = array + size - 1;
+
+    if (array && size && action)
     {
-        for (size_t i = 0; i < size; i++)
+        while (array <= end)
         {
-            action(array[i]);
+            action(*array++);
         }
     }
-}
-
-/**
- * print_elem - prints an integer
- * @elem: the integer to print
- *
- * Return: void
- */
-void print_elem(int elem)
-{
-    printf("%d\n", elem);
-}
-
-/**
- * print_elem_hex - prints an integer, in hexadecimal
- * @elem: the integer to print
- *
- * Return: void
- */
-void print_elem_hex(int elem)
-{
-    printf("0x%x\n", elem);
-}
-
-int main(void)
-{
-    int array[5] = {0, 98, 402, 1024, 4096};
-
-    printf("Printing array elements:\n");
-    array_iterator(array, 5, &print_elem);
-
-    printf("\nPrinting array elements in hexadecimal:\n");
-    array_iterator(array, 5, &print_elem_hex);
-
-    return 0;
 }
