6728e09180b21bb67118e95c2526e9906c6a717f
Adetunji Adeyimika <adadet@taltech.ee>
2023-06-22 02:49:50 +0300

    done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,28 +9,23 @@
  *
  * Return: always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    int num1, num2;
-    int (*op_func)(int, int);
+	int (*op_func)(int, int), a, b;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+		printf("Error\n"), exit(98);
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-    op_func = get_op_func(argv[2]);
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("Error\n"), exit(99);
 
-    printf("%d\n", op_func(num1, num2));
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
 
-    return (0);
+	printf("%d\n", op_func(a, b));
+	return (0);
 }
